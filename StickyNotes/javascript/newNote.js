5c1b6cea0cd090f343cebc72f4260a15aa5701d0
alicjasalamon <alicjasalamon@gmail.com>
2014-03-12 18:08:39 +0100

    first UI

---
+++ StickyNotes/javascript/newNote.js
@@ -0,0 +1,14 @@
+﻿$(document).ready(function () {
+    $('#board').click(function (e) {
+
+        var mainEvent = e ? e : window.event;
+
+        $('#board div:first')
+          .clone()
+          .append('clone')
+          .css('left', mainEvent.pageX)
+          .css('top', mainEvent.pageY - 25)
+          .appendTo($('#board'));
+
+    });
+});
---
+++ StickyNotes/javascript/showOptions.js
@@ -0,0 +1,36 @@
+﻿$(document).ready(function () {
+    $('#optionsButton').click(function () {
+
+        if ($('#sidebar').css('display') == 'none') {
+            $('#sidebar').show();
+        }
+        else
+        {
+            $('#sidebar').hide();
+        }
+        
+    });
+
+    $('#chalkBoard').click(function () {
+        
+       // $('body').css('background-image', 'url(' + "images/chalkboard.jpg" + ')');
+        // $(".note").css('background-image', 'url(' + "images/chalkNote.png" + ')');
+
+        $('body').animate({'background-image' : 'url(' + "images/chalkboard.jpg" + ')'}, 1000);
+        $(".note").animate({'background-image': 'url(' + "images/chalkNote.png" + ')'}, 1000);
+
+    });
+
+    $('#corkBoard').click(function () {
+
+        $('body').css('background-image', 'url(' + "images/corkboard.jpg" + ')');
+        $(".note").css('background-image', 'url(' + "images/note.png" + ')');
+    });
+
+
+/*    $('#fridgeBoard').click(function () {
+
+        $('body').css('background-image', 'url(' + "images/fridge.jpg" + ')');
+        $(".note").css('background-image', 'url(' + "images/fridgenote.png" + ')');
+    });*/
+});
